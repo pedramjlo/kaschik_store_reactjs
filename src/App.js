8dc183a6dfafd8d2e8e18f6b89745d7537d21d00
pedramjlo <pedram_sif@tuta.io>
2024-12-15 15:10:47 +0330

    added router dom

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+
+
+  return (
+    <Router>
+
+
+        <Routes>
+        <Route path='/' element={<Home/>} />
+      
+        </Routes>
+      </Router>
+
+  );
+}
+
+export default App;
